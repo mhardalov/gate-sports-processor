c5ccabc075c957cebc78dfab01c7b2c417543a74
Momchil Hardalov <momchil.hardalov@gmail.com>
2015-02-05 15:52:03 +0200

    Added DocumentModel class

---
+++ src/main/java/org/sports/gate/model/DocumentModel.java
@@ -0,0 +1,34 @@
+package org.sports.gate.model;
+
+import java.util.Date;
+
+public class DocumentModel {
+	private String url;
+	private Date date;
+	private String content;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
